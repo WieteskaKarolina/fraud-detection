6d99766b3fa592cd3147c2662d98eb95be1d4564
WieteskaKarolina <wieteskakarolina02@gmail.com>
2025-03-18 18:36:55 +0100

    Use lombok in transaction request dto.

--- src/main/java/com/example/dto/TransactionRequest.java
+++ src/main/java/com/example/dto/TransactionRequest.java
@@ -4,41 +4,23 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionRequest {
+
     @NotNull
     @Size(min = 6, max = 11, message = "BIN must be between 6 and 11 digits")
     @Pattern(regexp = "\\d+", message = "BIN must contain only digits")
-    public String bin;
+    private String bin;
 
     @Min(0)
-    public double amount;
+    private double amount;
 
     @NotNull
-    public String location;
-
-    public TransactionRequest(String bin, double amount, String location) {
-        this.bin = bin;
-        this.amount = amount;
-        this.location = location;
-    }
-
-    public TransactionRequest() {
-    }
-
-    public String getBin() {
-        return bin;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public void setBin(String bin) {
-        this.bin = bin;
-    }
+    private String location;
 }
